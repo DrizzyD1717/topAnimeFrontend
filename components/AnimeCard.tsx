3a2c4d4523041be5d53c7ff9ce63063947ffb47c
Olulana Dari <dariolulana@gmail.com>
2026-05-28 14:26:12 +0100

    Fixed Framer Motion TS error

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import Link from "next/link";
 
 // Animation variants for individual cards
-const itemVariants = {
+const itemVariants: Variants = {
   hidden: { opacity: 0, y: 30 },
   visible: {
     opacity: 1,
